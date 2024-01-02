fb59fc724208c3358abad2fd23723c92a3f56ddd
Andrew <andrewozo123@gmail.com>
2024-01-02 09:18:04 -0500

    Add project files.

---
+++ ClassesAndObjects/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Car
+    {
+        private string name;
+
+        public Car(string name)
+        {
+            this.name = name;
+            Console.WriteLine($"{this.name} was created");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{this.name} is Driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{this.name} is Stopped");
+        }
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassesAndObjects;
+
+
+
+
+Car audi = new Car("audi");
+
+audi.Drive();
+
+Console.WriteLine("Press 1 to stop the car");
+string? userInput = Console.ReadLine();
+
+if (userInput == "1")
+{
+    audi.Stop();
+}
